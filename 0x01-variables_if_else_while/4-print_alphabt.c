6fb62aa206bdb44068eadcc4d1943614983990fe
DadiW <dadwyfn@gmail.com>
2022-10-14 13:01:51 -0700

    letter except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * description: the lowercase exept q and e
+ * Return: value 0 for success
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' || ch != 'e')
+			putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}

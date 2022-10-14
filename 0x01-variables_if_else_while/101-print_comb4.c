3a63b71667c8f9a80b9aad4eef03bd904a772c8f
DadiW <dadwyfn@gmail.com>
2022-10-14 15:14:48 -0700

    three digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * description: three digit combination
+ * Return: value 0 for success
+ */
+int main(void)
+{
+	int ch, i, n;
+	for (ch = '0'; ch < '8'; ch++)
+	{
+		i = ch + 1;
+		while (i <= '8')
+		{
+			n = i + 1;
+			while (n <= '9')
+			{
+				putchar(ch);
+				putchar(i);
+				putchar(n);
+				if (i != '7')
+				{
+					putchar(44);
+					putchar(32);
+				}
+				n++;
+			}
+			i++;
+		}
+	}
+	putchar(10);
+	return (0);
+}

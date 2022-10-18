42055d8c39b824cc201339ea002f281882b47001
DadiW <dadwyfn@gmail.com>
2022-10-18 12:27:29 -0700

    lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main function
+ * description: lower case alphabet
+ * Return: value 0 for sucees
+ */
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+		putchar(10);
+}

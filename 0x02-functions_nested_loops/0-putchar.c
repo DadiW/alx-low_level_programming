0506f755a01fadb943becd5696632fb079fd5773
DadiW <dadwyfn@gmail.com>
2022-10-18 01:32:33 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - the main function
+ * description: prints putchar
+ * Return: valua 0 for success
+ */
+int main(void)
+{
+	char c;
+
+	c = write(1, "_putchar\n", 8);
+	return (0);
+}

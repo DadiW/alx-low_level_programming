5018661652ed7ff0823444bf634c31efc116db7d
DadiW <dadwyfn@gmail.com>
2022-10-19 12:41:01 -0700

    lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,6 +4,7 @@
 /**
  * main - main function
  * description: lower case alphabet
+ * print_alphabet - a function that prints the alphabet, in lowercase
  * Return: value 0 for sucees
  */
 void print_alphabet(void)

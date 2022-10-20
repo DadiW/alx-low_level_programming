ecb5bfadc0cbd0dc09638bb964bdfdb762b81c07
DadiW <dadwyfn@gmail.com>
2022-10-20 01:53:26 -0700

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,9 +8,9 @@ void print_times_table(int n)
 {
 	int i, r;
 
-	if (n >=0 && n <= 15)
+	if (n > 0 && n <= 15)
 	{
-		for (i = 0; i <=n; i++)
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
 			_putchar(',');

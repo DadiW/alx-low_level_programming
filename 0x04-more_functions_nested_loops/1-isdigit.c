a81ccf26d3a2f08ba02ff561983ce59c84bcf0ca
DadiW <dadwyfn@gmail.com>
2022-10-20 22:52:34 -0700

    Is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,9 +4,9 @@
  * @c: input
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _isgigit(int c)
+int _isdigit(int c)
 {
-	if ((c >= '0') && (c <= '9'))
+	if ((c >= 48) && (c <= 57))
 	{
 		return (1);
 	}

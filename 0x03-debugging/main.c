5f104340f310579cd74a97eaccfe41778832e7bd
DadiW <dadwyfn@gmail.com>
2022-10-21 04:52:03 -0700

    test

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main function
+ * description: test the validity 
+ * Return: value 0 for success
+ */
+int main(void)
+{
+	positive_or_negative(16);
+	return (0);
+
+}

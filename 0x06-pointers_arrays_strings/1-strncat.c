735c99b88bbaaddb99dc6167625f9a216d7a2428
DadiW <dadwyfn@gmail.com>
2022-10-27 01:50:40 -0700

    Concatinate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,8 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; (dest[i + j] = *src++) < n; j++)
+	for (j = 0; (dest[i + j] = src[j]) < n; j++)
 	{
+		if (src[j] == '\0')
+		{
+			j = n;
+		}
 	}
 	return (dest);
 }

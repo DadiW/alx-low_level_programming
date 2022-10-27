a8284fc9fcc83060775fa770faef91c793e231e9
DadiW <dadwyfn@gmail.com>
2022-10-27 10:06:07 -0700

    Rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rot13 - function
+ * @n: input
+ * Return: value for success
+ */
+char *rot13(char *n)
+{
+	int i, j;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (n[i] = a[j])
+			{
+				n[i] = b[j];
+			}
+		}
+	}
+}

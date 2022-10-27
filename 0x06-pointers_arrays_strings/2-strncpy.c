34f9f9305039c785849613f91105270cba11b932
DadiW <dadwyfn@gmail.com>
2022-10-27 02:28:47 -0700

    string copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,9 +10,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+       while (src[i] != '\0')
 	{
-		dest[i] = src[i];
+		if (i < n)
+			dest[i] = src[i];
+		i++;
 	}
+       for (;i < n;)
+	       dest[i] = '\0';
 	return (dest);
 }

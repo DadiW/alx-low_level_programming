74428941cf74d76a67521959803d046b2d3d90c4
DadiW <dadwyfn@gmail.com>
2022-11-19 10:48:34 -0800

    Name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints name + \n
+ * @argc: number of arguments typed
+ * @argv: array to arguements
+ * Return: Success
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
